fee6634f303936b548af8b251e53913c9ddebc85
Michał Golon <michal.golon@gmail.com>
2018-01-09 21:31:33 +0100

    Add Star Wars planets provider

---
+++ src/Provider/en_US/Planet.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace HydrefLab\JediFaker\Provider\en_US;
+
+use Faker\Provider\Base;
+
+class Planet extends Base
+{
+    /**
+     * @var array
+     */
+    protected static $planets = [
+        'Ahch-To', 'Alderaan', 'Bespin', 'Cantonica', 'Cato Neimoidia', 'Coruscant', 'D\'Qar', 'Dagobah', 'Endor',
+        'Felucia', 'Geonosis', 'Hosnian Prime', 'Hoth', 'Jakku', 'Kamino', 'Kashyyyk', 'Mustafar', 'Mygeeto', 'Naboo',
+        'Polis Massa', 'Starkiller Base', 'Takodana', 'Tatooine', 'Utapau', 'Yavin 4',
+    ];
+
+    /**
+     * @return string
+     */
+    public function planet(): string
+    {
+        return $this->generator->parse(static::randomElement(static::$planets));
+    }
+}
