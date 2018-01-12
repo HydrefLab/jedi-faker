9bb910a8009bcb8ea46b495a70250095e4dea83f
Michał Golon <michal.golon@gmail.com>
2018-01-12 14:23:18 +0100

    Add Star Wars faker factory

---
+++ src/Factory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace HydrefLab\JediFaker;
+
+use Faker\Factory as FakerFactory;
+use Faker\Generator;
+
+class Factory extends FakerFactory
+{
+    /**
+     * @var array
+     */
+    protected static $jediProviders = [
+        'Character', 'Droid', 'Movie', 'Planet', 'Species', 'Vehicle',
+    ];
+
+    /**
+     * @param string $locale
+     * @return Generator
+     */
+    public static function create($locale = self::DEFAULT_LOCALE): Generator
+    {
+        $generator = parent::create($locale);
+
+        foreach (static::$jediProviders as $provider) {
+            $providerClass = self::getProviderClass($provider, $locale);
+            $generator->addProvider(new $providerClass($generator));
+        }
+
+        return $generator;
+    }
+
+    /**
+     * @param string $provider
+     * @param string $locale
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    protected static function getProviderClass($provider, $locale): string
+    {
+        $providerClass = sprintf('HydrefLab\JediFaker\Provider\%s\%s', $locale, $provider);
+
+        if (true === class_exists($providerClass, true)) {
+            return $providerClass;
+        }
+
+        throw new \InvalidArgumentException(sprintf('Unable to find provider "%s" with locale "%s"', $provider, $locale));
+    }
+}
